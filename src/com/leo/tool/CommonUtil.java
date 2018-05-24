2e540b85c41512ca8b8122d7425a60e73d779d5f
Leo <18304097708@163.com>
2018-05-24 19:21:34 +0800

    123

--- src/com/leo/tool/CommonUtil.java
+++ src/com/leo/tool/CommonUtil.java
@@ -1,7 +1,11 @@
 package com.leo.tool;
 
 import java.util.UUID;
-
+/**
+ * Á¬½Ógit
+ * @author Administrator
+ *
+ */
 public class CommonUtil {
 	public static String getUUID() {
 		return UUID.randomUUID().toString();
